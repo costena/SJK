60878598faf31464c7879b6be65d225b777bd69a
costena <3224272275@qq.com>
2020-11-21 18:11:11 +0800

    czq

--- SJK/Session.cs
+++ SJK/Session.cs
@@ -1,24 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Common;
-using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
-using Google.Protobuf.WellKnownTypes;
-using MySql.Data;
 using MySql.Data.MySqlClient;
-using MySqlX.XDevAPI.Relational;
 
 namespace SJK
 {
     public class Session
     {
         private MySqlConnection connection;
+        private bool shortConnection = false;
         #region public methods
+        public Session(bool shortConnection = false)
+        {
+            this.shortConnection = shortConnection;
+        }
         public void Connect(string connectionString)
         {
+            connection?.Close();
             connection = new MySqlConnection(connectionString);
             connection.Open();
         }
@@ -123,7 +123,7 @@ namespace SJK
             List<string> conditions = new List<string>();
             foreach(var primaryKeyValue in primaryKeyValues)
             {
-                conditions.Add($"{primaryKeyValue.Key.KeyName} = {primaryKeyValue.Value.GetValue(record)}");
+                conditions.Add($"`{primaryKeyValue.Key.KeyName}` = '{primaryKeyValue.Value.GetValue(record)}'");
             }
             MySqlCommand sql = new MySqlCommand($"DELETE FROM `{table.TableName}` WHERE {string.Join(" AND ", conditions)}", connection);
             return sql.ExecuteNonQuery();
@@ -153,11 +153,11 @@ namespace SJK
             {
                 if(keyValue.Key is PrimaryKeyAttribute)
                 {
-                    conditions.Add($"{keyValue.Key.KeyName} =  {keyValue.Value.GetValue(record)}");
+                    conditions.Add($"`{keyValue.Key.KeyName}` = '{keyValue.Value.GetValue(record)}'");
                 }
                 else
                 {
-                    sets.Add($"{keyValue.Key.KeyName} = {keyValue.Value.GetValue(record)}");
+                    sets.Add($"`{keyValue.Key.KeyName}` = '{keyValue.Value.GetValue(record)}'");
                 }
             }
             MySqlCommand sql = new MySqlCommand($"UPDATE `{table.TableName}` SET {string.Join(", ", sets)} WHERE {string.Join(" AND ", conditions)}", connection);
@@ -185,19 +185,28 @@ namespace SJK
             return await sql.ExecuteNonQueryAsync();
         }
 
-        public IEnumerable<T> Find<T>(params Condition[] conditions)
+        public T[] Find<T>(params Condition[] conditions)
         {
             CheckConnect();
             TableAttribute table = typeof(T).GetCustomAttribute<TableAttribute>();
             string[] conditionStrings = new string[conditions.Length];
-            for(int i = 0; i < conditions.Length; i++)
+            MySqlCommand sql;
+            if (conditionStrings.Length > 0)
             {
-                conditionStrings[i] = conditions[i].ToString();
+                for (int i = 0; i < conditions.Length; i++)
+                {
+                    conditionStrings[i] = conditions[i].ToString();
+                }
+                sql = new MySqlCommand($"SELECT * FROM `{table.TableName}` WHERE {string.Join(" AND ", conditionStrings)}", connection);
             }
-            MySqlCommand sql = new MySqlCommand($"SELECT * FROM `{table.TableName}` WHERE {string.Join(" AND ", conditionStrings)}", connection);
+            else
+            {
+                sql = new MySqlCommand($"SELECT * FROM `{table.TableName}`", connection);
+            }
             MySqlDataReader reader = sql.ExecuteReader();
             try
             {
+                List<T> ts = new List<T>();
                 while (reader.Read())
                 {
                     T t = Activator.CreateInstance<T>();
@@ -206,8 +215,9 @@ namespace SJK
                     {
                         keyValue.Value.SetValue(t, reader[keyValue.Key.KeyName]);
                     }
-                    yield return t;
+                    ts.Add(t);
                 }
+                return ts.ToArray();
             }
             finally
             {
@@ -245,11 +255,19 @@ namespace SJK
             CheckConnect();
             TableAttribute table = typeof(T).GetCustomAttribute<TableAttribute>();
             string[] conditionStrings = new string[conditions.Length];
-            for (int i = 0; i < conditions.Length; i++)
+            MySqlCommand sql = null;
+            if (conditions.Length > 0)
             {
-                conditionStrings[i] = conditions[i].ToString();
+                for (int i = 0; i < conditions.Length; i++)
+                {
+                    conditionStrings[i] = conditions[i].ToString();
+                }
+                sql = new MySqlCommand($"SELECT * FROM `{table.TableName}` WHERE {string.Join(" AND ", conditionStrings)} LIMIT 1", connection);
             }
-            MySqlCommand sql = new MySqlCommand($"SELECT * FROM `{table.TableName}` WHERE {string.Join(" AND ", conditionStrings)}", connection);
+            else
+            {
+                sql = new MySqlCommand($"SELECT * FROM `{table.TableName}` LIMIT 1", connection);
+            }
             MySqlDataReader reader = sql.ExecuteReader();
             T result = Activator.CreateInstance<T>();
             if (reader.Read())
